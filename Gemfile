8f98abba7103ac93ba21e3927dd3d134ba1c2e84
elenarumi <extra.madri@gmail.com>
2025-03-27 16:54:10 +0100

    Subiendo el proyecto de Appium con Ruby

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'appium_lib', '~> 8.1.0'
+gem 'selenium-webdriver', '~> 2.53.4'
+gem 'rspec'
---
+++ login-test.rb
@@ -0,0 +1,45 @@
+require 'appium_lib'
+
+
+caps = {
+  platformName: 'Mac',  #
+  browserName: 'Chrome'
+  
+}
+
+appium_driver = Appium::Driver.new({caps: caps}, true)
+driver = appium_driver.start_driver
+
+begin
+
+  driver.get("https://www.abc.es/")
+
+
+  sleep 3
+
+  links = driver.find_elements(:tag_name, 'a')
+
+  puts "🔍 Se encontraron #{links.size} enlaces en la página."
+
+  links.each_with_index do |link, index|
+    begin
+      url = link.attribute('href')
+      if url && url.start_with?('http') # Solo abrir enlaces válidos
+        puts "➡️ Abriendo enlace #{index + 1}: #{url}"
+        driver.get(url)
+        sleep 2 # Esperar que cargue la página
+        driver.navigate.back # Volver a la página principal
+        sleep 2
+      end
+    rescue StandardError => e
+      puts "❌ Error al abrir enlace #{index + 1}: #{e.message}"
+    end
+  end
+
+  puts "✅ Prueba completada con éxito"
+
+rescue StandardError => e
+  puts "❌ Error: #{e.message}"
+ensure
+  driver.quit
+end
